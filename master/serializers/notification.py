c125c81d02cf22923050dd167f514ec9a7982d64
ajay-navsoft <ajay.singh@gmail.com>
2021-07-12 16:28:39 +0530

    change model

--- master/serializers/notification.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from mobility_apps.master.models import Notification
-
-class NotificationSerializers(serializers.ModelSerializer):
-    class Meta:
-        model =  Notification
-        # fields = ('firstname','lastname')
-        fields = '__all__'
-
--- master/serializers/role.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from mobility_apps.master.models import Role
-
-class RoleSerializers(serializers.ModelSerializer):
-    class Meta:
-        model =  Role
-        # fields = ('firstname','lastname')
-        fields = '__all__'
-
